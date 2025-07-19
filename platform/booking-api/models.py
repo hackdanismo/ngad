c237fea5ec14821b340d108a9711331c52322688
hackdanismo <office@justtoconfirm.co.uk>
2025-07-19 16:05:27 +0100

    feat: add a new field to the models.py file to store the date the user wishes to make a booking

--- platform/booking-api/models.py
+++ platform/booking-api/models.py
@@ -6,5 +6,7 @@ class Booking(db.Model):
     __tablename__ = 'booking'   # optional, but explicit
     id = db.Column(db.Integer, primary_key=True)
     company_id = db.Column(db.Integer, nullable=False)
+    # Store the specific date the user wants to make a booking
+    date = db.Column(db.Date, nullable=False)
     timestamp = db.Column(db.DateTime, server_default=db.func.now())
     note = db.Column(db.String, nullable=True)
